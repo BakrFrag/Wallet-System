b61d37554dede4df3eaa637766c12e9565e5de68
Abou Bakr Frag Mohamed Bahi Eldeen <AbouBakr.Frag@Egypt.TE-Data.core>
2021-09-18 17:26:01 +0200

    add regex validators on wallet model for wallet number and wallet password

--- WalletSystem/wallet/models.py
+++ WalletSystem/wallet/models.py
@@ -1,12 +1,13 @@
 from django.db import models
 from django.db.models.fields import FloatField
-
+from django.core.validators import RegexValidator;
+from .db_validators import *
 # Create your models here.
 class Wallet(models.Model):
     """
     include all nessasry fields for creating , mainuplations wallet
     """
-    phone=models.CharField(max_length=13);
+    phone=models.CharField(max_length=13,validators=[RegexValidator(phone_number_pattern,"wallet phone number must be 11 digits")]);
     balance=models.FloatField(default=0.0);
-    password = models.CharField(max_length=6);
+    password = models.CharField(max_length=6,validators=[RegexValidator(wallet_password_pattern,"wallet password must be 6 numbers")]);
     is_active = models.BooleanField(default=False)
