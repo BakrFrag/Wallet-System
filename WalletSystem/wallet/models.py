e0a54c5f4150de778235be98bd38d5560079c095
Abou Bakr Frag Mohamed Bahi Eldeen <AbouBakr.Frag@Egypt.TE-Data.core>
2021-09-18 17:16:09 +0200

    add wallet model to wallet app

--- WalletSystem/wallet/models.py
+++ WalletSystem/wallet/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.db.models.fields import FloatField
 
 # Create your models here.
+class Wallet(models.Model):
+    """
+    include all nessasry fields for creating , mainuplations wallet
+    """
+    phone=models.CharField(max_length=13);
+    balance=models.FloatField(default=0.0);
+    password = models.CharField(max_length=6);
+    is_active = models.BooleanField(default=False)
